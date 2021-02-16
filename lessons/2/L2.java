64b4eeae1e20d2b445836982ffd0f8dfa1e673cb
Mansur Ziiatdinov <gltronred@gmail.com>
2021-02-16 11:03:32 +0300

    List methods

--- lessons/2/L2.java
+++ lessons/2/L2.java
@@ -10,19 +10,35 @@ class Elem {
     }
 }
 
-public class L2 {
-    public static void main(String[] args){
-        Elem root = new Elem(4,
-                    new Elem(5,
-                    new Elem(6,
-                    null)));
+class List {
+    Elem head;
 
-        // print list
-        Elem p = root;
+    public List() {
+        head = null;
+    }
+    public void print() {
+        Elem p = head;
         while (p != null) {
-            System.out.println(p.value);
+            System.out.print(p.value + " ");
             p = p.next;
         }
-       
+        System.out.println();
+
+    }
+    public void add(int value) {
+        head = new Elem(value, head);
+    }
+}
+
+public class L2 {
+    public static void main(String[] args){
+        List list1 = new List();
+        list1.add(6);
+        list1.add(5);
+        list1.add(4);
+        list1.print();
+
+        list1.add(3);
+        list1.print();
     }
 }
