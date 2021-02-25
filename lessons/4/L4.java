ddccd6e0697e2bbf12a0ddfccf3c9ee7d8b9c04e
Mansur Ziiatdinov <gltronred@gmail.com>
2021-02-25 14:43:14 +0300

    4. Задание попроще

--- lessons/4/L4.java
+++ lessons/4/L4.java
@@ -4,12 +4,20 @@ public class L4 {
     // Оставить в списке только элементы, которые делятся на 3,
     // после которых стоит элемент, который делится на 5
     //
-    // >>> 3 5 2 6 15 25 0 10 1 4
+    // >>> 3 5 2 6 15 25 0 10 1 6 4
     // 3 6 15 0
     public static void f1(LinkedList<Integer> list) {
         return;
     }
-    public static void main(String[] args) {
+
+    // Оставить в списке только элементы, которые делятся на 3
+    //
+    // >>> 3 5 2 6 15 25 0 10 1 6 4
+    // 3 6 15 0 6
+    public static void f2(LinkedList<Integer> list) {
+        return;
+    }
+    public static LinkedList<Integer> mkList() {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(3);
         list.add(5);
@@ -20,12 +28,23 @@ public class L4 {
         list.add(0);
         list.add(10);
         list.add(1);
+        list.add(6);
         list.add(4);
-
-        f1(list);
+        return list;
+    }
+    public static void printList(List<Integer> list) {
         for (Integer el : list) {
             System.out.print(el + " ");
         }
         System.out.println();
+    }
+    public static void main(String[] args) {
+        List<Integer> list = mkList();
+        f1(list);
+        printList(list);
+
+        list = mkList();
+        f2(list);
+        printList(list);
     }
 }
