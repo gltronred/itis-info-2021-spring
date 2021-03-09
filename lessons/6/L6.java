bb6400ecc9ccb65c283c1d1e4bd8ee35184de13d
Mansur Ziiatdinov <gltronred@gmail.com>
2021-03-09 11:02:26 +0300

    Слияние списков по два

--- lessons/6/L6.java
+++ lessons/6/L6.java
@@ -4,7 +4,41 @@ public class L6 {
     // Дан список, состоящий из упорядоченных по возрастанию списков
     // Вернуть результат их слияния
     public static List<Integer> f1(List<List<Integer>> list) {
-       return new LinkedList<>();
+        List<Integer> r = new LinkedList<>();
+        for (List<Integer> l : list) {
+            r = f2(r,l);
+        }
+        return r;
+    }
+
+    // слияние двух списков
+    public static List<Integer> f2(List<Integer> l1, List<Integer> l2) {
+        List<Integer> r = new LinkedList<>();
+        ListIterator<Integer> i = l1.listIterator();
+        ListIterator<Integer> j = l2.listIterator();
+        while (i.hasNext() && j.hasNext()) {
+            int x = i.next();
+            int y = j.next();
+            if (x < y) {
+                // добавляем меньший элемент
+                r.add(x);
+                // возвращаем указатель в другом списке назад
+                j.previous();
+            } else {
+                // добавляем меньший элемент
+                r.add(y);
+                // возвращаем указатель в другом списке назад
+                i.previous();
+            }
+        }
+        // добавляем элементы из более длинного списка
+        while (i.hasNext()) {
+            r.add(i.next());
+        }
+        while (j.hasNext()) {
+            r.add(j.next());
+        }
+        return r;
     }
 
     public static void printList(List<Integer> list) {
