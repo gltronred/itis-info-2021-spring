2ee9968b99a8ebd7c0062317825299663dd33e71
Mansur Ziiatdinov <gltronred@gmail.com>
2021-03-16 10:41:34 +0300

    Заготовки для 2 и 3

--- lessons/8/L8.java
+++ lessons/8/L8.java
@@ -16,12 +16,38 @@ class Transaction {
 
 public class L8 {
     public static int computeBalance(List<Transaction> transactions, String who) {
-        return 0;
+        int balance = 0;
+        for (Transaction txn : transactions) {
+            if (txn.getFrom().equals(who)) {
+                balance -= txn.getAmount();
+            }
+            if (txn.getTo().equals(who)) {
+                balance += txn.getAmount();
+            }
+        }
+        return balance;
+    }
+    public static Map<String,Integer> computeBalances(List<Transaction> transactions) {
+        return new HashMap<>();
+    }
+    public static void printPositive(Map<String,Integer> balances) {
+        for (Map.Entry<String,Integer> entry : balances.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
     public static void main(String[] args) {
         List<Transaction> txns = new LinkedList<Transaction>();
         txns.add(new Transaction("Alice","Bob",10));
         txns.add(new Transaction("Bob","Charlie",5));
+        // 1
         System.out.println(computeBalance(txns, "Bob"));
+        // 2
+        Map<String,Integer> balances = computeBalances(txns);
+        for (Map.Entry<String,Integer> entry : balances.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        // 3
+        System.out.println("Who increased balance:");
+        printPositive(balances);
     }
 }
