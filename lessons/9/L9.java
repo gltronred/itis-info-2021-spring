e98ae1db8c33385ed62d0b32dd5cb4ac289cb583
Mansur Ziiatdinov <gltronred@gmail.com>
2021-03-18 14:26:11 +0300

    Заготовка

---
+++ lessons/9/L9.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class L9 {
+    static class Transaction {
+        private int seconds;
+        public int getTime() { return seconds; }
+        private String from;
+        public String getFrom() { return from; }
+        private String to;
+        public String getTo() { return to; }
+        private String currency;
+        public String getCurrency() { return currency; }
+        private int amount;
+        public int getAmount() { return amount; }
+        public Transaction(int start, String from, String to, String currency, int amount) {
+            this.seconds = start;
+            this.from = from;
+            this.to = to;
+            this.currency = currency;
+            this.amount = amount;
+        }
+    }
+
+    static class Price {
+        private int time;
+        public int getTime() { return time; }
+        private String currency;
+        public String getCurrency() { return currency; }
+        private int price;
+        public int getPrice() { return price; }
+        public Price(int time, String currency, int price) {
+            this.time = time;
+            this.currency = currency;
+            this.price = price;
+        }
+    }
+
+    public static Map<String,Integer> computeBalances(List<Price> prices, List<Transaction> txns) {
+        return new TreeMap<>();
+    }
+
+    public static void print(Map<String,Integer> balances) {
+        for (Map.Entry<String,Integer> entry : balances.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Transaction> txns = new LinkedList<Transaction>();
+        txns.add(new Transaction(10,"Alice","Bob",   "USD",  10));
+        txns.add(new Transaction(20,"Bob","Charlie", "USD",   5));
+        txns.add(new Transaction(20,"Dave","Charlie","EUR",   5));
+        txns.add(new Transaction(50,"Eve","Frank",   "RUB", 500));
+
+        List<Price> prices = new LinkedList<>();
+        prices.add(new Price(0,"USD",75));
+        prices.add(new Price(0,"EUR",90));
+        prices.add(new Price(0,"RUB",1));
+        prices.add(new Price(18,"USD",80));
+        prices.add(new Price(19,"EUR",80));
+        prices.add(new Price(40,"EUR",90));
+        prices.add(new Price(41,"USD",75));
+        prices.add(new Price(49,"RUB",2));
+
+        // 1
+        Map<String,Integer> balances = computeBalances(prices,txns);
+    }
+}
