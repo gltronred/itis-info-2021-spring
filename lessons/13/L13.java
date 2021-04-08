4bcc27f2d12fb206070c743df37d575cac56b92d
Mansur Ziiatdinov <gltronred@gmail.com>
2021-04-08 15:25:33 +0300

    Решение

--- lessons/13/L13.java
+++ lessons/13/L13.java
@@ -3,32 +3,40 @@ import java.util.stream.*;
 
 public class L13 {
     public static boolean isPrime(int n) {
-        return false;
+        long m = Stream
+            .iterate(1, x -> x+1)
+            .limit((long)Math.sqrt(n))
+            .filter(x -> n % x == 0)
+            .count();
+        return m==1 && n!=1;
     }
     public static void main(String[] args) {
-        long n = Stream
-            .iterate(1, x -> x+1)
-            .map(x -> x+10)
-            .filter(x -> x % 2 == 1)
-            .limit(10)
-            .map(x -> {
-                    System.out.println(x);
-                    return x;
-                })
-            .count();
-        System.out.println("n=" + n);
+        Stream.of(1,2,5,15,49,101)
+            .forEach(x -> System.out.println(x + " - " + isPrime(x)));
 
-        Stream
-            .of(1,2,3,4)
-            .map(x -> x*x)
-            .forEach(x -> System.out.println(x));
+        // long n = Stream
+        //     .iterate(1, x -> x+1)
+        //     .map(x -> x+10)
+        //     .filter(x -> x % 2 == 1)
+        //     .limit(10)
+        //     .map(x -> {
+        //             System.out.println(x);
+        //             return x;
+        //         })
+        //     .count();
+        // System.out.println("n=" + n);
 
-        System.out.println("Randoms");
+        // Stream
+        //     .of(1,2,3,4)
+        //     .map(x -> x*x)
+        //     .forEach(x -> System.out.println(x));
 
-        Random r = new Random();
-        Stream
-            .generate(() -> r.nextInt(15))
-            .limit(15)
-            .forEach(x -> System.out.println(x));
+        // System.out.println("Randoms");
+
+        // Random r = new Random();
+        // Stream
+        //     .generate(() -> r.nextInt(15))
+        //     .limit(15)
+        //     .forEach(x -> System.out.println(x));
     }
 }
