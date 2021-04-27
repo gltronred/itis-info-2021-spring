0b0f470f1145bde9a04cf0d61e29c41b5dc2bba2
Mansur Ziiatdinov <gltronred@gmail.com>
2021-04-27 11:06:54 +0300

    Файлы на глубине 1

---
+++ lessons/16/L16.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.*;
+
+public class L16 {
+    public static void main(String[] args) throws IOException {
+        // передаём путь, где нужно
+        // работать, используя аргументы
+        // командной строки (args[0])
+        Files.list(Paths.get(args[0]))
+            .flatMap(p -> {
+                    try {
+                        if (Files.isRegularFile(p))
+                            return Stream.of(p);
+                        else
+                            return Files.list(p);
+                    } catch (IOException e) {
+                        return Stream.empty();
+                    }
+                })
+            .filter(Files::isRegularFile)
+            .forEach(System.out::println);
+    }
+}
