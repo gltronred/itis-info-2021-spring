21c581f5cead9e15c7a98e29aa9e0126766802b2
Mansur Ziiatdinov <gltronred@gmail.com>
2021-04-27 11:22:35 +0300

    Комментарии к решению

--- lessons/16/L16.java
+++ lessons/16/L16.java
@@ -8,6 +8,12 @@ public class L16 {
         // работать, используя аргументы
         // командной строки (args[0])
         Files.list(Paths.get(args[0]))
+            // для каждого пути возвращаем Stream
+            // - для файла - сам этот файл
+            // - для папки - содержимое папки
+            // - если ошибка - пустой Stream
+            // ошибка ловится, поскольку иначе
+            // не скомпилируется
             .flatMap(p -> {
                     try {
                         if (Files.isRegularFile(p))
@@ -18,10 +24,16 @@ public class L16 {
                         return Stream.empty();
                     }
                 })
+            // оставляем только те пути,
+            // которые обычные файлы (не папки)
             .filter(Files::isRegularFile)
+            // оставляем только те пути,
+            // имя файла в которых
+            // заканчивается на .java
             .filter(p -> p.getFileName()
                         .toString()
                         .endsWith(".java"))
+            // выводим каждый такой путь
             .forEach(System.out::println);
     }
 }
